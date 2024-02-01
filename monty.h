339fd15e186ec8ff02c6a149b8137f3cca843046
Stephani6G <stephaniegloria283@gmail.com>
2024-01-31 22:26:42 -0800

    monty.h

--- monty.h
+++ monty.h
@@ -60,7 +60,8 @@ void func_sub(stack_t **head, unsigned int counter);
 void func_mul(stack_t **head, unsigned int counter);
 void func_div(stack_t **head, unsigned int counter);
 void func_mod(stack_t **head, unsigned int counter);
-
+void frees_stack(stack_t *head);
+void fu_stack(stack_t **head, unsigned int counter);
 
 
 #endif /* monty_h */
