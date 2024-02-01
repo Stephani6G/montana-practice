8034e03bec05edecfcd5dd6aa16cbe4c881a0d57
Stephani6G <stephaniegloria283@gmail.com>
2024-01-31 21:54:26 -0800

    submo.c

---
+++ submo.c
@@ -0,0 +1,60 @@
+#include "monty.h"
+
+/**
+* func_sub - function that substracts nodes
+* @head: double head pointer to the stack
+* @counter: line count
+*/
+void func_sub(stack_t **head, unsigned int counter)
+{
+	stack_t *temp;
+	int sub, nd;
+
+	temp = *head;
+	for (nd = 0; temp != NULL; nd++)
+		temp = temp->next;
+	if (nd < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		frees_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	temp = *head;
+	sub = temp->next->n - temp->n;
+	temp->next->n = sub;
+	*head = temp->next;
+	free(temp);
+}
+
+/**
+* func_mul - function that multiplies the top two elements of the stack
+* @head: double head pointer to the stack
+* @counter: line count
+*/
+void func_mul(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int length = 0, temp;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		frees_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	temp = h->next->n * h->n;
+	h->next->n = temp;
+	*head = h->next;
+	free(h);
+}
