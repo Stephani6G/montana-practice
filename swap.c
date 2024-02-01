cffc2639967d0662049bb608ab86812afdca8651
Stephani6G <stephaniegloria283@gmail.com>
2024-01-31 23:18:10 -0800

    swap.c

---
+++ swap.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+
+/**
+* func_pall - function that prints everything in the stack
+* @head: double head pointer to the stack
+* @counter: unused line count
+*/
+void func_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
+
+/**
+* func_swap - function that swaps the top two elements of the stack
+* @head: head of stack
+* @counter: line count
+*/
+void func_swap(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int length = 0, temp;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(bus.filem);
+		free(bus.content);
+		frees_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	temp = h->n;
+	h->n = h->next->n;
+	h->next->n = temp;
+}
